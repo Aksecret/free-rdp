f442696f2f802ad11fd3efb83a1f5e3bcccefde8
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-11-18 20:53:43 +0530

    Create Logo2.sh

---
+++ OnlineHacKing/Logo2.sh
@@ -0,0 +1,20 @@
+echo -e "\e[1;96m================================================================== > > >\e[m "
+echo " "
+printf "\e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;44m VNC Server RDP Linux \e[0m                     
+echo
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"     
+echo
+printf "\e[1;31m[\e[0m\e[1;92m06\e[0m\e[1;31m] \e[0m\e[1;91;107m Ubuntu Desktop lxde \e[0m  "        
+echo
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
+echo
+printf "\e[1;31m[\e[0m\e[1;92m07\e[0m\e[1;31m] \e[0m\e[1;34;103m Coming soon \e[0m "             
+echo
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"  
+echo
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"  
+echo
+printf "\e[1;31m[\e[0m\e[1;92m99\e[0m\e[1;31m] \e[0m\e[1;33mExit\e[0m\n"
+echo " "
+echo -e "\e[1;96m================================================================== > > >\e[m "
+echo ""
