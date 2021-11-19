84d9e362c8f30fcf6f3ef4f5dda42b1e8c55b411
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-11-19 20:20:31 +0530

    Update Logo2.sh

--- OnlineHacKing/Logo2.sh
+++ OnlineHacKing/Logo2.sh
@@ -11,11 +11,12 @@ echo -e ""
    echo -e $''
    echo ""
       echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ >>\e[0m'
-      echo -e "\e[93m  VNC Server RDP Linux  "      
-      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ >>\e[0m' 
-      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+ >>\e[0m'
-      echo -e "\e[95m  Ubuntu Desktop lxde "      
-      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+ >>\e[0m' 
+      echo -e "\e[93m  VNC Server RDP Linux \e[0m "      
+      echo -e $'' 
+      echo -e "\e[95m  Ubuntu Desktop lxde \e[0m " 
+      echo -e $'' 
+      echo -e " \e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;44m X Suit \e[0m "      
+      echo -e $' ' 
       echo ""                                                
       }
        banner
