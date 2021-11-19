84e0903653af5ede92c12514891a32958210820f
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-11-19 16:48:06 +0530

    Update Logo2.sh

--- OnlineHacKing/Logo2.sh
+++ OnlineHacKing/Logo2.sh
@@ -1,15 +1,13 @@
-echo "\e[31m[\e[0m\e[92m01\e[0m\e[31m] \e[0m\e[1;37;44m VNC Server RDP Linux \e[0m                     
+echo "\e[31m[\e[0m\e[92m01\e[0m\e[31m] \e[0m\e[37m VNC Server RDP Linux \e[0m                     
 echo
-echo "\e[31m\e[0m\e[92m\e[0m\e[31m \e[0m\e[1;37;44m\e[0m"     
+echo "\e[31m\e[0m\e[92m\e[0m\e[31m \e[0m\e[37m\e[0m"     
 echo
-echo "\e[31m[\e[0m\e[92m06\e[0m\e[31m] \e[0m\e[1;91;107m Ubuntu Desktop lxde \e[0m  "        
+echo "\e[31m[\e[0m\e[92m06\e[0m\e[31m] \e[0m\e[91m Ubuntu Desktop lxde \e[0m  "        
 echo
-echo "\e[31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
+echo "\e[31m\e[0m\e[92m\e[0m\e[31m \e[0m\e[37m\e[0m"
 echo
-echo "\e[31m[\e[0m\e[1;92m07\e[0m\e[1;31m] \e[0m\e[1;34;103m Coming soon \e[0m "             
+echo "\e[31m[\e[0m\e[92m07\e[0m\e[31m] \e[0m\e[34m Coming soon \e[0m "             
 echo
-echo "\e[31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"  
+echo "\e[31m\e[0m\e[92m\e[0m\e[31m \e[0m\e[37m\e[0m"   
 echo
-echo "\e[31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"  
-echo
-echo "\e[31m[\e[0m\e[1;92m99\e[0m\e[1;31m] \e[0m\e[1;33mExit\e[0m\n"
+echo "\e[31m[\e[0m\e[92m99\e[0m\e[31m] \e[0m\e[33mExit\e[0m\n"
