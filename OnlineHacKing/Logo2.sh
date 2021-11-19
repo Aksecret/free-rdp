ea5a4162fc939dee254c6f4280c73d6bdbfead13
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-11-19 19:42:58 +0530

    Update Logo2.sh

--- OnlineHacKing/Logo2.sh
+++ OnlineHacKing/Logo2.sh
@@ -1,13 +1,7 @@
-echo "\e[31m[\e[0m\e[92m01\e[0m\e[31m] \e[0m\e[37m VNC Server RDP Linux \e[0m                     
-echo
-echo "\e[31m\e[0m\e[92m\e[0m\e[31m \e[0m\e[37m\e[0m"     
-echo
-echo "\e[31m[\e[0m\e[92m06\e[0m\e[31m] \e[0m\e[91m Ubuntu Desktop lxde \e[0m  "        
-echo
-echo "\e[31m\e[0m\e[92m\e[0m\e[31m \e[0m\e[37m\e[0m"
-echo
-echo "\e[31m[\e[0m\e[92m07\e[0m\e[31m] \e[0m\e[34m Coming soon \e[0m "             
-echo
-echo "\e[31m\e[0m\e[92m\e[0m\e[31m \e[0m\e[37m\e[0m"   
-echo
-echo "\e[31m[\e[0m\e[92m99\e[0m\e[31m] \e[0m\e[33mExit\e[0m\n"
+echo ""
+echo " \e[96m VNC Server RDP Linux \e[0m    "                 
+echo ""
+echo " \e[96m Ubuntu Desktop lxde \e[0m  "     
+echo ""
+echo " \e[96m Coming Soon \e[0m "     
+echo ""
