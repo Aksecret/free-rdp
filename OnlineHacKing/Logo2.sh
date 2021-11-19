a944a057911b041c183a116335a83563d02948e8
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-11-19 21:08:41 +0530

    Update Logo2.sh

--- OnlineHacKing/Logo2.sh
+++ OnlineHacKing/Logo2.sh
@@ -9,16 +9,16 @@ echo ""
 banner() {
 echo -e ""
       echo -e $'\e[1;34m================================================================== > > >\e[0m '
+            echo -e $'' 
+      echo -e " \e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;41m VNC Server RDP Linux \e[0m "    
       echo -e $'' 
-      echo -e " \e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;44m VNC Server RDP Linux \e[0m "    
+      echo -e " \e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;91;107m Ubuntu Desktop lxde \e[0m    " 
       echo -e $'' 
-      echo -e " \e[1;31m[\e[0m\e[1;92m06\e[0m\e[1;31m] \e[0m\e[1;91;107m Ubuntu Desktop lxde \e[0m    " 
-      echo -e $'' 
-      echo -e " \e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;37;41m Coming Soon \e[0m    " 
+      echo -e " \e[1;31m[\e[0m\e[1;92m03\e[0m\e[1;31m] \e[0m\e[1;37;44m Coming Soon \e[0m " 
       echo -e $' '
       echo -e $' '
       echo -e " \e[1;31m[\e[0m\e[1;92m99\e[0m\e[1;31m] \e[0m\e[1;33mExit\e[0m\n   "
-      echo -e $' ' 
+      echo -e $' '  
       echo -e $'\e[1;34m================================================================== > > >\e[0m '
       echo -e $' ' 
       echo ""                                                
