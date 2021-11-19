847c2ea92ae0c8bfe5a855d838a08367b57a8d95
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-11-19 20:26:46 +0530

    Update Logo2.sh

--- OnlineHacKing/Logo2.sh
+++ OnlineHacKing/Logo2.sh
@@ -8,15 +8,13 @@ yellow='\e[1;33m'
 echo ""
 banner() {
 echo -e ""
-   echo -e $''
-   echo ""
       echo -e $'\e[1;96m================================================================== > > >\e[0m '
       echo -e $'' 
       echo -e " \e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;44m VNC Server RDP Linux \e[0m "    
       echo -e $'' 
       echo -e " \e[1;31m[\e[0m\e[1;92m06\e[0m\e[1;31m] \e[0m\e[1;91;107m Ubuntu Desktop lxde \e[0m    " 
       echo -e $'' 
-      echo -e " \e[1;31m[\e[0m\e[1;92m07\e[0m\e[1;31m] \e[0m\e[1;34;103m Cuming soon \e[0m     " 
+      echo -e " \e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;37;41m Coming Soon \e[0m    " 
       echo -e $' '
       echo -e $' '
       echo -e "\e[1;31m[\e[0m\e[1;92m99\e[0m\e[1;31m] \e[0m\e[1;33mExit\e[0m\n   "
