4e2a8e7fd817281cec81aed02dbe0a0494ce0a4b
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-11-23 13:36:58 +0530

    Update Logo2.sh

--- OnlineHacKing/Logo2.sh
+++ OnlineHacKing/Logo2.sh
@@ -10,11 +10,11 @@ banner() {
 echo -e ""
       echo -e $'\e[1;34m================================================================== > > >\e[0m '
             echo -e $'' 
-      echo -e " \e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;41m Debian Linux VNC Server \e[0m "    
+      echo -e " \e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;41m Debian Linux Server \e[0m "    
       echo -e $'' 
-      echo -e " \e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;48;5;21m Debian Desktop  \e[0m    " 
+      echo -e " \e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;48;5;21m Ubuntu Desktop lxde  \e[0m    " 
       echo -e $'' 
-      echo -e " \e[1;31m[\e[0m\e[1;92m03\e[0m\e[1;31m] \e[0m\e[1;91;107m Ubuntu Desktop lxde  \e[0m " 
+      echo -e " \e[1;31m[\e[0m\e[1;92m03\e[0m\e[1;31m] \e[0m\e[1;91;107m Cuming soon....  \e[0m " 
       echo -e $' '
       echo -e $' '
       echo -e " \e[1;31m[\e[0m\e[1;92m99\e[0m\e[1;31m] \e[0m\e[1;33mExit\e[0m\n   "
