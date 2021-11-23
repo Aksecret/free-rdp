284d3820ff53fbe0b1103d969236acc68182bbc7
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-11-23 13:09:18 +0530

    Update Debian-Linux-ChomeDesktop.sh

--- RDP/Debian-Linux-ChomeDesktop.sh
+++ RDP/Debian-Linux-ChomeDesktop.sh
@@ -24,9 +24,9 @@
 			   printf " \e[1;31m[\e[0m\e[1;92m03\e[0m\e[1;31m]  \e[92m Copy \e[96m Debian Linux \e[0m \e[92m and Paste on terminal/shell google console \e[0m " 
 			   echo " "
 			   printf ' \e[1;31m[\e[0m\e[1;92m04\e[0m\e[1;31m]  \e[31m[\e[32m*\e[31m]\e[33m Enter The Debian Linux Code : \e[0m'
-			   
 			   echo ""
 			   printf " \e[1;31m[\e[0m\e[1;92m05\e[0m\e[1;31m]  \e[91m Click this Linux :- \e[97m https://remotedesktop.google.com/access \e[0m " 
+			   echo ""
 			   
 			   echo -e "\e[96m=====================================================================\e[92m"
                            echo ""
