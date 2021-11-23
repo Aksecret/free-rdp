4b85c06e95e01bf67fe152b3f8f3533f4519ad41
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-11-23 16:43:52 +0530

    Create 0.sh

---
+++ OnlineHacKing/0.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo ""
+echo ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m  !!       Welcome to Online HacKing      !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ---------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo -e ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m How To Install And Use This Tool Play Video \e[0m"
+echo ""
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Video Link :\e[0m\e[1;36m https://play.onlinehacking.xyz/v/7ue43a  "
+echo ""
+echo ""
+echo ""
+read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Video Watching Complete ? \e[1;91m (Y/N) : \e[0m' option
+echo""
+echo""
+echo""
+if [[ $option == *'N'* ]]; then
+clear
+exit
+fi
+if [[ $option == *'n'* ]]; then
+clear
+exit
+fi
+clear
+echo ""
