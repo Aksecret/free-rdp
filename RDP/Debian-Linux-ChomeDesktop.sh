d6b6991ce37f3f70f59c6204da1f266d8c20d0af
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-11-23 17:11:45 +0530

    Update Debian-Linux-ChomeDesktop.sh

--- RDP/Debian-Linux-ChomeDesktop.sh
+++ RDP/Debian-Linux-ChomeDesktop.sh
@@ -2,13 +2,17 @@
 			   echo ""
          printf "\e[31m[\e[32m★\e[31m]\e[32m This Cloud Desktop VNC Server RDP \e[m "
          echo ""
+	 echo -e
+	 echo -e
 			   sudo apt update -y 
 			   sudo apt install xfce4 xfce4-goodies xrdp -y
 			   sudo apt install -y tightvncserver -y
 			   sudo apt install firefox-esr -y
          echo ""
+	 echo -e
 			   printf "\e[1;31m[\e[0m\e[1;92mSUMAN\e[0m\e[1;31m] \e[0m\e[1;37;41m Setup Server \e[0m   "  
 			   echo " "
+			   echo -e
 			   echo ""
 			   echo ""
 			   sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -27,7 +31,7 @@
 			   echo ""
 			   printf " \e[1;31m[\e[0m\e[1;92m05\e[0m\e[1;31m]  \e[91m Click this Linux :- \e[97m https://remotedesktop.google.com/access \e[0m " 
 			   echo ""
-			   
+			   echo -e
 			   echo -e "\e[96m=====================================================================\e[92m"
                            echo ""
          echo ""
