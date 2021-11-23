9a1d9fb0d5afa5b9236caffe1253c337aa914770
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-11-23 17:12:45 +0530

    Update ubuntu-desktop-lxde-vnc.sh

--- RDP/ubuntu-desktop-lxde-vnc.sh
+++ RDP/ubuntu-desktop-lxde-vnc.sh
@@ -4,6 +4,7 @@ echo ""
                            sudo apt install -y tightvncserver -y
 			   sudo apt update -y
 			   printf "\e[1;31m[\e[0m\e[1;92mSUMAN\e[0m\e[1;31m] \e[0m\e[1;37;41m Setup Server \e[0m   "  
+			   echo -e
 			   sudo apt install docker
                            echo ""
 			   clear
@@ -14,10 +15,13 @@ echo ""
 	 echo ""
 			   echo ""
 			   echo ""
+			   echo -e
 			   printf "\e[31m[\e[32m★\e[31m]\e[32m This Cloud Server is Ready \e[m "
 			   echo""
+			   echo -e
 			   printf '\e[91m Start Server and Click Link :- \e[97m https://localhost:8080 \e[0m '
 			   echo ""
+			   echo -e
                            read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Start VNC Server \e[1;91m (Y/N) : \e[0m' option
                            echo""
                            echo""
@@ -33,7 +37,8 @@ echo ""
 			   echo " "     
 			   echo ""
 			   echo
-			   echo ""			   
+			   echo ""
+			   echo -e
                            read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Stop Server \e[1;91m (Y/N) : \e[0m' option
                            echo""
                            echo""
