cd0b2bf0512ebebb13481d36c033e59d36095c1f
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-11-23 12:44:58 +0530

    Update Debian-Linux-ChomeDesktop.sh

--- RDP/Debian-Linux-ChomeDesktop.sh
+++ RDP/Debian-Linux-ChomeDesktop.sh
@@ -19,12 +19,12 @@
 	 echo ""
 			   printf ' \e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m]  \e[93m Click thsi Linux :- \e[97m https://remotedesktop.google.com/headless \e[0m '
 			   echo ""
-			   printf " \e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m]   \e[92m Click Begin , Click Before , Click Authorize \e[0m " 
+			   printf " \e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m]  \e[92m Click Begin , Click Before , Click Authorize \e[0m " 
 			   echo " "
-			   printf " \e[1;31m[\e[0m\e[1;92m03\e[0m\e[1;31m]   \e[92m copy \e[96m"Debian Linux"\e[0m \e[92m and paste on terminal/shell google console \e[0m " 
+			   printf " \e[1;31m[\e[0m\e[1;92m03\e[0m\e[1;31m]  \e[92m Copy \e[96m Debian Linux \e[0m \e[92m and Paste on terminal/shell google console \e[0m " 
 			   echo " "
-			   printf ' \e[1;31m[\e[0m\e[1;92m04\e[0m\e[1;31m]   \e[31m[\e[32m*\e[31m]\e[33m Enter The Debian Linux Code : \e[0m'
+			   printf ' \e[1;31m[\e[0m\e[1;92m04\e[0m\e[1;31m]  \e[31m[\e[32m*\e[31m]\e[33m Enter The Debian Linux Code : \e[0m'
 			   echo ""
-			   printf " \e[1;31m[\e[0m\e[1;92m05\e[0m\e[1;31m]   \e[93m Click thsi Linux :- \e[97m https://remotedesktop.google.com/access \e[0m " 
+			   printf " \e[1;31m[\e[0m\e[1;92m05\e[0m\e[1;31m]  \e[93m Click this Linux :- \e[97m https://remotedesktop.google.com/access \e[0m " 
 			   echo ""
          echo""
