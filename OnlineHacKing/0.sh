af60473d93066b1c0712132a77d7bc3dfa31602d
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-11-23 17:08:52 +0530

    Update 0.sh

--- OnlineHacKing/0.sh
+++ OnlineHacKing/0.sh
@@ -7,7 +7,7 @@ echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m  !!       Welcome to O
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ---------------------------------------- \e[1;91m\e[0m'
 echo ""
 echo -e ""
-echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m How To Install And Use This Tool Play Video \e[0m"
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m How To Install And Use This Cloud Play Video \e[0m"
 echo ""
 echo -e
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Video Link :\e[0m\e[1;36m https://play.onlinehacking.xyz/  "
